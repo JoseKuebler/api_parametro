472fe57189bdf46514b52c80cd2e65f94cc486c8
Jose Kuebler <josekuebler@Kuebler-M1.local>
2023-02-12 15:17:53 -0300

    dockerfile

--- apiparametro/apiparametro/settings.py
+++ apiparametro/apiparametro/settings.py
@@ -127,9 +127,11 @@ USE_TZ = True
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 VENV_PATH = os.path.dirname(BASE_DIR)
-STATIC_ROOT = os.path.join(VENV_PATH, 'static_root')
+STATIC_ROOT = os.path.join(VENV_PATH, 'staticfiles')
